91268b21a389246fb80263e195482979d5b73f1a
t <tysonpederson95@gmail.com>
2022-08-16 09:29:59 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    // Return error if start or end are negative or not a number
+    if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
+    
+    // Switch start and end if start is larger than end
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    
+    // Sum all numbers between and including start and end
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
