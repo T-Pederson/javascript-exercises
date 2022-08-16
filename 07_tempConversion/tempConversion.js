08d7a60ffe5e3f6f89b76e316d9d9dbb86aa9f7e
t <tysonpederson95@gmail.com>
2022-08-16 09:51:38 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  // Convert fahrenheit to celcius
+  let c = (f - 32) * 5 / 9;
 
+  // Round to 1 decimal place
+  c = parseFloat((Math.round(c * 10) / 10).toFixed(1));
+
+  return c;
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  // Convert celcius to fahrenheit
+  let f = c * 9 / 5 + 32;
 
+  // Round to 1 decimal place
+  f = parseFloat((Math.round(f * 10) / 10).toFixed(1));
+
+  return f;
 };
 
 // Do not edit below this line
