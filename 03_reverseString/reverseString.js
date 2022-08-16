a1d9aa79980f024a125ce79d7613714413860197
t <tysonpederson95@gmail.com>
2022-08-15 20:58:58 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    string = string.split('');
+    let stringLength = string.length;
+    let newString = [];
+    for (let i = stringLength - 1, j = 0; i >=0; i--, j++) {
+        newString[j] = string[i]
+    }
+    newString = newString.join('');
+    return newString;
 
 };
 
