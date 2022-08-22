9d2549ce033f3aae8db9711eb13b1647d353ffad
t <tysonpederson95@gmail.com>
2022-08-22 14:56:43 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    words = words.toLowerCase();
+    words = words.replace(/[!,.\s]/g, "");
+    let newWords = "";
+    for(let i = words.length - 1; i >= 0; i--) {
+        newWords += words[i];
+    }
+    if (newWords === words) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
