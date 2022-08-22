aaab95192ad351871f99746c839d465f2a7c7323
t <tysonpederson95@gmail.com>
2022-08-22 13:27:11 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  for(let number of numbers) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 0;
+  for(let number of numbers) {
+    if (numbers.indexOf(number) === 0) {
+      total += number;
+    } else {
+      total *= number;
+    }
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = a;
+  for(let i = 1; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  for(let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
