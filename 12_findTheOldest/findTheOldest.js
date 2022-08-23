f788947c568b32ebe6aa5364ce780d4cde63390c
t <tysonpederson95@gmail.com>
2022-08-23 13:21:16 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestAge = 0;
+    let oldestPerson = {};
+    for(obj of array) {
+        if ("yearOfDeath" in obj === false) {
+            let currentYear = new Date().getFullYear();
+            if (currentYear - obj.yearOfBirth > oldestAge) {
+                oldestAge = currentYear - obj.yearOfBirth;
+                oldestPerson = obj;
+            }
+        } else if (obj.yearOfDeath - obj.yearOfBirth > oldestAge) {
+                oldestAge = obj.yearOfDeath - obj.yearOfBirth;
+                oldestPerson = obj;
+            }
+        }
+    return oldestPerson;
 };
 
 // Do not edit below this line
