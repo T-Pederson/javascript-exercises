c6ae99d7103701ea866d710684f7917f993c86c9
t <tysonpederson95@gmail.com>
2022-08-23 11:20:20 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) {
+        return "OOPS";
+    } else if(number == 0) {
+        return 0;
+    } else {
+        let firstNumber = 0;
+        let secondNumber = 1;
+        let tempNumber = 1;
+        for(let i = 1; i < number; i++) {
+            tempNumber = secondNumber;
+            secondNumber += firstNumber;
+            firstNumber = tempNumber;
+        }
+        return secondNumber;
+    }
 
 };
 
